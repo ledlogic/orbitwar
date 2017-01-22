8548a644bb4411c3834a61f304d668c5d32d9d61
Jeff Conrad <ledlogic@gmail.com>
2017-01-22 16:34:12 -0600

    Added row calculations

--- js/orbit.js
+++ js/orbit.js
@@ -3,6 +3,7 @@ var orbit = {
 	$orbittable: null,
 	$map: null,
 	dataPath: "data",
+	mode: "data",
 	unitHdrModule: "unit-hdr",
 	unitsFile: "data/orbit-war.csv",
 	unitsHdr: null,
@@ -22,6 +23,7 @@ var orbit = {
 		queue.push("orbit.redraw();");
 		
 		$("#search").on("textchange", orbit.search);
+		$(".orbit-mode-menu li").on("click", orbit.modeMenuChanged);
 		
 		queue.next();
 	},
@@ -77,6 +79,7 @@ var orbit = {
 	
 	renderForm: function() {
 		orbit.log("render form");
+		orbit.log("orbit.mode[" + orbit.mode + "]");
 		
 		var h = [];
 
@@ -88,8 +91,19 @@ var orbit = {
 		for (var i=0; i<orbit.unitsHdr.length; i++) {
 			var key = orbit.unitsHdr[i];
 			var value = messageResource.get(key, orbit.unitHdrModule);
-			th += "<th>" + value + "</th>";
+			
+			var colspan = 1;
+			if (orbit.mode === "calc" && key.indexOf("-cost") > -1) {
+				colspan = 2;
+			}
+			
+			th += "<th colspan=\"" + colspan + "\">"
+				+ value
+				+ "</th>";
 		}
+		if (orbit.mode == "calc") {
+			th += "<th>Total</th>";
+		}				
 		th += "</tr>";
 		th += "</thead>";
 		h.push(th);
@@ -100,21 +114,34 @@ var orbit = {
 		for (var i=0; i<orbit.unitsData.length; i++) {
 			var d = orbit.unitsData[i];
 			if (d) {
-				var u = "img/units/" + d["hdr.unit.type"] + ".fw.png";
-				var t = d["hdr.unit.type"];
+				var u = "img/units/" + d["hdr-unit-type"] + ".fw.png";
+				var t = d["hdr-unit-type"];
 				
 				if (t) {
-					th += "<tr>";
+					th += "<tr class=\"orbit-row-" + j + "\">";
 					th += "<td>"
 						+ "<img src=\"" + u + "\" alt=\"" + t + "\" class=\"unit-type\" />"
 					    + "</td>";
 					for (var j in d) {
 						var dj = d[j];
-						if (j == "hdr.unit.type") {
+						if (j == "hdr-unit-type") {
 							dj = dj.toUpperCase();
 						}
 						th += "<td>" + dj + "</td>";
+						
+						orbit.log("j[" + j + "]");
+						if (orbit.mode == "calc" && j.indexOf("-cost") > -1) {
+							if ($.isNumeric(dj)) {
+								th += "<td><input type=\"number\" class=\"orbit-calc orbit-calc-" + i + "-" + j + "\" size=\"3\" min=\"0\" pattern=\"^[0-9]\" /></td>";								
+							} else {
+								th += "<td>&nbsp;</td>";
+							}
+						}
 					}
+					
+					if (orbit.mode == "calc") {
+						th += "<td><input type=\"text\" class=\"orbit-calc orbit-calc-row-" + i +"\" size=\"5\" /></td>";
+					}					
 					th += "</tr>";
 				}
 			}
@@ -122,7 +149,7 @@ var orbit = {
 		th += "</tbody>";
 		h.push(th);
 
-		orbit.$orbittable.append(h.join());
+		orbit.$orbittable.html(h.join());
 		
 		$('.unit-type').on('click', function() {
 			var alt = $(this).attr('alt');
@@ -133,6 +160,16 @@ var orbit = {
 			$("#complete-dialog .imagepreview").attr("src", src);
 			$("#complete-dialog .modal-title").html(alt);
 			$('#complete-dialog').modal('show');
+		});
+		
+		// TODO: debounce
+		$(".orbit-calc").on('textchange', function() {
+			var v0 = $(this).val();
+			var v1 = v0.replace(/[^0-9]/g, '');
+			if (v1 != v0) {
+				$(this).val(v1);
+			}
+			orbit.recalc();
 		});
 		
 		queue.next();
@@ -147,13 +184,13 @@ var orbit = {
 		
 		var hh = $heading.height() + parseInt($heading.css("padding-top"), 10) + parseInt($heading.css("padding-bottom"), 10);
 		var fh = $footer.height() + parseInt($footer.css("padding-top"), 10) + parseInt($footer.css("padding-bottom"), 10);
-		//hex.hexLog("hh[" + hh + "]");
-		//hex.hexLog("fh[" + fh + "]");
+
 		$body.css("top", hh + "px");
 		$body.css("bottom", fh + "px");
 	},
 	
 	search: function() {
+		orbit.log("search");
 		var $search = $("#search");
 		var s = $search.val().toLowerCase();
 		$("#orbittable tbody tr").each(function() {
@@ -165,6 +202,46 @@ var orbit = {
 				that.addClass("search-not-found");
 			}
 		});
+	},
+	
+	modeMenuChanged: function() {
+		orbit.log("modeMenuChanged");
+		$("ul.orbit-mode-menu li").removeClass("active");
+		var mode = $(this).data("mode");
+		orbit.log("mode[" + mode + "]");
+		var active = $("ul.orbit-mode-menu li.mode-menu-" + mode);
+		active.addClass("active");
+		orbit.log(["active", active]);
+		orbit.mode = mode;
+		orbit.renderForm();
+		orbit.recalc();
+	},
+	
+	recalc: function() {
+		orbit.log("recalc");
+		if (orbit.mode == "calc") {
+			for (var i=0; i<orbit.unitsData.length; i++) {
+				var rowCost = 0;
+				var d = orbit.unitsData[i];
+				if (d) {
+					for (var j in d) {
+						var dj = d[j];
+						if (j.indexOf("-cost") > -1) {
+							var classRef = ".orbit-calc-" + i + "-" + j;
+							var val = $(classRef).val();
+							var q = parseInt(val, 10);
+							q = q ? q : 0;
+							var cost = parseInt(dj, 10);
+							cost = cost ? cost : 0;
+							//orbit.log("i[" + i + "], d[" + d + "], j[" + j + "], dj[" + dj + "], classRef[" + classRef + "], q[" + q + "], cost[" + cost + "]");
+							rowCost += q * cost;
+						}
+					}					
+					var ccrow = ".orbit-calc-row-" + i;
+					$(ccrow).val(rowCost);
+				}
+			}
+		}
 	}
 };
 
